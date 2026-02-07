3c90e2d8fe3fa9eb84b885d48085654c0886601d
erlint1212 <e.t.nordtvedt@gmail.com>
2026-02-07 19:40:22 +0100

    Inital templ and Go server setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	srv := server.NewServer()
+	mux := server.RegisterRoutes()
+	srv := server.NewServer(mux)
 
 	log.Printf("Serving on: http://localhost%s/\n", srv.Addr)
 	log.Fatal(srv.ListenAndServe())
